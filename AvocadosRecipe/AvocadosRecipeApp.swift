9778a59cb5737ac291a57ce326f7e2ffdf871861
Lexter Tapawan <lexter.tapawan@gmail.com>
2023-11-21 10:26:44 +0000

    Initial Commit

---
+++ AvocadosRecipe/AvocadosRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosRecipeApp.swift
+//  AvocadosRecipe
+//
+//  Created by Lexter Tapawan on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvocadosRecipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AvocadosRecipe
+//
+//  Created by Lexter Tapawan on 21/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
